090d6446b2f276d73acea00cab33cef2edebf1ef
ljh-none <144912155+ljh-none@users.noreply.github.com>
2024-01-10 00:06:04 +0900

    init

---
+++ pointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("=================\n");
+    printf("포인터 변수의 크기 체크\n");
+    printf("sizeof(char *) : %lu\n", sizeof(char *));
+    printf("sizeof(int *) : %lu\n", sizeof(int *));
+    printf("sizeof(double *) : %lu\n", sizeof(double *));
+    printf("sizeof(unsigned int *) : %lu\n", sizeof(unsigned int *));
+    
+    int a = 10;
+    
+}
